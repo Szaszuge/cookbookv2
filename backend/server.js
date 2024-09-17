207d7315847a07ca6f8b2f8bd3a21da13c180c10
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2024-09-17 12:26:16 +0200

    crypto-js jól áll rajtad bébe

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,24 @@ const uuid = require('uuid');
 var cors = require('cors');
 var CryptoJS = require("crypto-js");
 var moment = require('moment');
+
+const app = express();
+const port = process.env.PORT;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+var pool  = mysql.createPool({
+    connectionLimit : process.env.CONNECTIONLIMIT,
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME
+  });
+
+//get API version
+app.get('/', (req, res) => {
+    res.send(`API version : ${process.env.VERSION}`);
+  });
